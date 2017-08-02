e77ed5347d1447d25bea87c0bf6ae8b981b6d3e3
Monte <monte.hazboun@gmail.com>
2017-08-02 14:51:41 -0400

    first commit

---
+++ DSCResources/cWinRMListener/cWinRMListener.psm1
@@ -0,0 +1,154 @@
+﻿Function Get-Targetresource {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Transport, 
+
+        [Parameter(Mandatory = $true)]
+        [String]$port,
+
+        [Parameter()]
+        [String]$Address = '*',
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [String]$Ensure
+    )
+
+
+    $winrmdata = winrm e winrm/config/listener
+    $winrmObject = Convert-WinRMDataToObject $winrmdata
+
+    $listener = $winrmObject | where { ($_.transport -eq $Transport) -and ($_.port -eq $port) -and ($_.Address -eq $address) }
+    $presence = if ($listener) {"Present"} else {"Absent"}
+
+    return @{
+        Transport = $Transport
+        Port      = $port
+        Address   = $Address
+        Ensure    = $presence
+    }
+}
+
+
+Function Test-Targetresource {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Transport, 
+
+        [Parameter(Mandatory = $true)]
+        [String]$port,
+
+        [Parameter()]
+        [String]$Address = '*',
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [String]$Ensure
+    )
+
+
+    $winrmdata = winrm e winrm/config/listener
+    $winrmObject = Convert-WinRMDataToObject $winrmdata
+
+    $listener = $winrmObject | where { ($_.transport -eq $Transport) -and ($_.port -eq $port) -and ($_.Address -eq $Address) } 
+    switch ($Ensure) {
+        "Present" {
+            if ($listener) {
+                return $true  
+            } else {
+                return $false
+            }
+        }
+        "Absent" {
+            if ($listener) {
+                return $false 
+            } else {
+                return $true
+            }
+        }
+    }
+}
+
+Function Set-Targetresource {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("HTTP","HTTPs",IgnoreCase=$true)]
+        [String]$Transport, 
+
+        [Parameter(Mandatory = $true)]
+        [String]$port,
+
+        [Parameter()]
+        [String]$Address = '*',
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [String]$Ensure
+    )
+
+
+    switch ($ensure) {
+        "Present" {
+            if ($Transport -eq "HTTP") {
+                if ($port -eq "80") {
+                    $exp = "`@`{EnableCompatibilityHttpListener=`"true`"`}"
+                    winrm set winrm/config/service $exp    
+                } else {
+                    winrm quickconfig -q
+                    winrm delete winrm/config/listener?Address=$address+Transport=$transport
+                    winrm create winrm/config/listener?Address=$address+Transport=$transport        
+                }    
+            }
+            if ($transport -eq "HTTPs") {
+                winrm quickconfig -transport:https -q    
+            }
+        }
+        "Absent" {
+            if ($port -eq 80) {
+                $exp = "`@`{EnableCompatibilityHttpListener=`"false`"`}"
+                winrm set winrm/config/service $exp    
+            } else {
+                winrm delete winrm/config/listener?Address=$address+Transport=$transport        
+            }    
+         }
+     }
+}
+
+Function Convert-WinRMDataToObject {
+    param (
+        [string[]]$winrmdata
+    )
+
+    $winRMarray = @()
+
+    foreach ($line in $winrmdata) {
+        if ($line -like "Listener*") {
+            $object = New-Object -TypeName psobject -Property @{
+                Name = $line
+                Address = ""
+                Transport = ""
+                Port = ""
+                Enabled = ""
+                URLPrefix = ""
+                ListeningOn = ""
+            }
+            continue
+        }
+        
+        if ($line -like "*Hostname*" -or $line -like "*CertificateThumbprint*") {continue}
+        if ($line -eq "") {
+            $winRMarray+=$object
+        } else {
+            $linesplit = $line.Split("=").TrimStart("    ")
+            $name = $linesplit[0].replace(" ","")
+            $value = $linesplit[1].replace(" ","")
+            $object.$name = $value
+        }              
+    
+    }
+    return $winRMarray
+}
+
+Export-ModuleMember *-TargetResource
---
+++ DSCResources/cWinRMService/cWinRMService.psm1
@@ -0,0 +1,207 @@
+﻿Function Get-Targetresource {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$SettingName, 
+
+        [Parameter(Mandatory = $true)]
+        [String]$SettingValue,
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [String]$Ensure
+    )
+
+
+    $winrmdata = winrm get winrm/config/service
+    $winrmObject = Convert-WinRMDataToObject $winrmdata
+
+    switch ($SettingName) {
+        "CredSSP"           { $presence = if ($winrmObject.Auth.$settingname -eq $SettingValue) {"Present"} else {"Absent"} }
+        "Kerberos"          { $presence = if ($winrmObject.Auth.$settingname -eq $SettingValue) {"Present"} else {"Absent"} }
+        "Negotiate"         { $presence = if ($winrmObject.Auth.$settingname -eq $SettingValue) {"Present"} else {"Absent"} }
+        "Basic"             { $presence = if ($winrmObject.Auth.$settingname -eq $SettingValue) {"Present"} else {"Absent"} }
+        "cbthardeninglevel" { $presence = if ($winrmObject.Auth.$settingname -eq $SettingValue) {"Present"} else {"Absent"} }
+        "Certificate"       { $presence = if ($winrmObject.Auth.$settingname -eq $SettingValue) {"Present"} else {"Absent"} }
+        "HTTP"              { $presence = if ($winrmObject.DefaultPorts.$settingname -eq $SettingValue) {"Present"} else {"Absent"} }
+        "HTTPS"             { $presence = if ($winrmObject.DefaultPorts.$settingname -eq $SettingValue) {"Present"} else {"Absent"} }
+        default             { $presence = if ($winrmObject.$settingname -eq $SettingValue) {"Present"} else {"Absent"} }
+    }
+
+    return @{
+        SettingName  = $SettingName
+        SettingValue = $SettingValue
+        Ensure       = $presence
+    }
+}
+
+
+Function Test-Targetresource {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$SettingName, 
+
+        [Parameter(Mandatory = $true)]
+        [String]$SettingValue,
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [String]$Ensure
+    )
+
+
+    $winrmdata = winrm g winrm/config/service
+    $winrmObject = Convert-WinRMDataToObject $winrmdata
+
+    switch ($SettingName) {
+        "CredSSP"           { if ($winrmObject.Auth.$settingname -eq $SettingValue) {return $true} else {return $false} }
+        "Kerberos"          { if ($winrmObject.Auth.$settingname -eq $SettingValue) {return $true} else {return $false} }
+        "Negotiate"         { if ($winrmObject.Auth.$settingname -eq $SettingValue) {return $true} else {return $false} }
+        "Basic"             { if ($winrmObject.Auth.$settingname -eq $SettingValue) {return $true} else {return $false} }
+        "cbthardeninglevel" { if ($winrmObject.Auth.$settingname -eq $SettingValue) {return $true} else {return $false} }
+        "Certificate"       { if ($winrmObject.Auth.$settingname -eq $SettingValue) {return $true} else {return $false} }
+        "HTTP"              { if ($winrmObject.DefaultPorts.$settingname -eq $SettingValue) {return $true} else {return $false} }
+        "HTTPS"             { if ($winrmObject.DefaultPorts.$settingname -eq $SettingValue) {return $true} else {return $false} }
+        default             { if ($winrmObject.$settingname -eq $SettingValue) {return $true} else {return $false} }
+    }
+}
+
+Function Set-Targetresource {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$SettingName, 
+
+        [Parameter(Mandatory = $true)]
+        [String]$SettingValue,
+
+        [Parameter()]
+        [ValidateSet("Present", "Absent")]
+        [String]$Ensure
+    )
+
+
+    switch ($ensure) {
+        "Present" {
+                switch ($SettingName) {
+                    "CredSSP"           { $runme = "`@`{$SettingName=`"$SettingValue`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                                      
+                    }
+                    "Kerberos"          { $runme = "`@`{$SettingName=`"$SettingValue`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                                       
+                    }
+                    "Negotiate"         { $runme = "`@`{$SettingName=`"$SettingValue`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                                     
+                    }
+                    "Basic"             { $runme = "`@`{$SettingName=`"$SettingValue`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                    
+                    }
+                    "cbthardeninglevel" { $runme = "`@`{$SettingName=`"$SettingValue`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                                        
+                    }
+                    "Certificate"       { $runme = "`@`{$SettingName=`"$SettingValue`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                            
+                    }
+                    "HTTP"              { $runme = "`@`{$SettingName=`"$SettingValue`"`}"
+                                          winrm set winrm/config/service/DefaultPorts $runme # | Out-Null
+                    }
+                    "HTTPS"             { $runme = "`@`{$SettingName=`"$SettingValue`"`}"
+                                          winrm set winrm/config/service/DefaultPorts $runme # | Out-Null           
+                    }
+                    default             { $runme = "`@`{$SettingName=`"$SettingValue`"`}"
+                                          winrm set winrm/config/service $runme # | Out-Null
+                    }
+                }
+
+        }
+        "Absent" {
+                switch ($SettingName) {
+                    "CredSSP"           { $runme = "`@`{$SettingName=`"`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                                      
+                    }
+                    "Kerberos"          { $runme = "`@`{$SettingName=`"`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                                       
+                    }
+                    "Negotiate"         { $runme = "`@`{$SettingName=`"`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                                     
+                    }
+                    "Basic"             { $runme = "`@`{$SettingName=`"`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                    
+                    }
+                    "cbthardeninglevel" { $runme = "`@`{$SettingName=`"`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                                        
+                    }
+                    "Certificate"       { $runme = "`@`{$SettingName=`"`"`}"
+                                          winrm set winrm/config/service/auth $runme # | Out-Null                            
+                    }
+                    "HTTP"              { $runme = "`@`{$SettingName=`"`"`}"
+                                          winrm set winrm/config/service/DefaultPorts $runme # | Out-Null
+                    }
+                    "HTTPS"             { $runme = "`@`{$SettingName=`"`"`}"
+                                          winrm set winrm/config/service/DefaultPorts $runme # | Out-Null           
+                    }
+                    default             { $runme = "`@`{$SettingName=`"`"`}"
+                                          winrm set winrm/config/service $runme # | Out-Null
+                    }
+                }   
+         }
+     }
+}
+
+Function Convert-WinRMDataToObject {
+    param (
+        [string[]]$winrmdata
+    )
+
+    $object = New-Object -TypeName psobject -Property @{
+        RootSDDL = ""
+        MaxConcurrentOperations = ""
+        MaxConcurrentOperationsPerUser = ""
+        EnumerationTimeoutms = ""
+        MaxConnections = ""
+        MaxPacketRetrievalTimeSeconds = ""
+        AllowUnencrypted = ""
+        Auth = @{
+            Basic = ""
+            Kerberos = ""
+            Negotiate = ""
+            Certificate = ""
+            CredSSP = ""
+            cbthardeninglevel = ""
+        }
+        DefaultPorts = @{
+            HTTP = ""
+            HTTPS = ""
+        }
+        IPv4Filter = ""
+        IPv6Filter = ""
+        EnableCompatibilityHttpListener = ""
+        EnableCompatibilityHttpsListener = ""
+        AllowRemoteAccess = ""
+    }
+
+    $propertyNames = ($object | Get-Member -MemberType NoteProperty).Name
+          
+    foreach ($line in $winrmdata) {
+        $linesplit = $line.Split("=").TrimStart("    ")
+        if ($linesplit.count -gt 1) {
+            $name = $linesplit[0].replace(" ","")
+            $value = $linesplit[1].replace(" [Source","").replace(" ","")
+        }
+
+        if ($propertyNames -contains $name) {
+            $object.$name = $value
+        } else {
+            if ($object.Auth.Keys -contains $name) {
+                $object.Auth.$name = $value
+            }
+            if ($object.DefaultPorts.Keys -contains $name) {
+                $object.DefaultPorts.$name = $value
+            }
+        } 
+    }            
+    return $object
+}
+
+Export-ModuleMember *-TargetResource
